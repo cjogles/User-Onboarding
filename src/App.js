ddd66b46ba01dbc5a811377dca6da42e7d8b139f
cjogles <jackson.ogles28@gmail.com>
2019-10-17 18:36:32 -0600

    finished mvp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Onboard from './components/Onboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Onboard />
+    </div>
+  );
+}
+
+export default App;
